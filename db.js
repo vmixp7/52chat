1ca6bcf83b1c69c37cfbc6e0a44b860b4e011db2
arlen_lin <vmixp7@gmail.com>
2017-12-17 02:17:22 +0800

    db1

--- db.js
+++ db.js
@@ -44,4 +44,14 @@ mongoose.model( 'room', room );
 mongoose.model( 'cate', cate );
 mongoose.model( 'chat', chat );
 
-mongoose.connect( 'mongodb://arlen:2iuixigi@localhost:27017/chat_db' );
+// mongoose.connect( 'mongodb://arlen:2iuixigi@localhost:27017/chat_db' );
+var promise = mongoose.createConnection('mongodb://arlen:2iuixigi@localhost:27017/chat_db', {
+  useMongoClient: true,
+});
+promise.then(function(db) {
+  /* Use `db`, for instance `db.model()`
+});
+// Or, if you already have a connection
+connection.openUri('mongodb://localhost/myapp', { /* options */
+});
+// mongoose.connect( 'mongodb://arlen:2iuixigi@localhost:27017/chat_db' );
