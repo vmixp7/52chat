9e2ce5cc98519426eca5fe323476f8238028551a
arlen_lin <vmixp7@gmail.com>
2017-12-17 06:04:04 +0800

    955

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ var mongoose = require( 'mongoose' );
 var connection = mongoose.createConnection('mongodb://localhost:27017/chat_db', {
   useMongoClient: true,
 });
-var User = connection.model('user',user);
+var User = connection.model('user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
